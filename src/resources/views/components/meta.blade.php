b1ab4ecf8360c306c6046e1c24c4b99ba0daab5f
pondol <y.h.ryu@tipszone.co.kr>
2025-11-26 12:25:11 +0900

    t
    t

--- src/resources/views/components/meta.blade.php
+++ src/resources/views/components/meta.blade.php
@@ -2,24 +2,26 @@
 @php
     [$meta, $og, $twitter] = $meta->toArray();
 @endphp
-
-{{-- Canonical 태그 자동 출력 로직 --}}
 @if (isset($__data['meta']->canonical) && $__data['meta']->canonical)
     <link rel="canonical" href="{{ $__data['meta']->canonical }}" />
 @endif
-{{-- Canonical 태그 자동 출력 로직 --}}
-
 {{-- SEO Meta Tags --}}
 @foreach ($meta as $k => $v)
-    <meta name="{{ $k }}" content="{{ $v }}" />
+    @if ($k !== 'canonical')
+        <meta name="{{ $k }}" content="{{ $v }}" />
+    @endif
 @endforeach
 
 {{-- Open Graph Meta Tags --}}
 @foreach ($og as $k => $v)
     <meta property="og:{{ $k }}" content="{{ $v }}" />
 @endforeach
-<meta property="og:url" content="{{ request()->fullUrl() }}" />
-
+{{-- 
+    [SEO 꿀팁 수정] og:url 통일 
+    : request()->fullUrl()은 파라미터(?a=b)를 포함하지만, 
+      SEO를 위해 Canonical과 동일하게 파라미터를 제거한 url()->current()를 권장합니다.
+--}}
+<meta property="og:url" content="{{ url()->current() }}" />
 {{-- Twitter Card Meta Tags --}}
 @foreach ($twitter as $k => $v)
     <meta name="twitter:{{ $k }}" content="{{ $v }}" />
